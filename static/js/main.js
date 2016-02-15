cc8d004959206dd033884ccdc79ee7d87e29710d
Steve Scavo <stevescavo@gmail.com>
2016-02-14 23:02:55 -0800

    Update `src` files, remove compass dependency

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-require(['./config'], function() {
-    require(['app/app'], function(app) {
-        // If the module is a function, execute it...
-        // Otherwise do nothing
-        if (typeof app === 'function') {
-            app();
-        }
-    });
-});
