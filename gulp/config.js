d1ffd7279a5261df8d48aa5d915ee7b5003f0391
Steve Scavo <stevescavo@gmail.com>
2017-12-10 00:01:59 -0800

    Build `client` bundle

--- gulp/config.js
+++ gulp/config.js
@@ -6,44 +6,50 @@ var settings_json;
 try {
     settings_json = require('../settings');
 } catch (e) {
-    console.log('settings.json file not found, some tasks may not work without this.');
+    console.log(
+        'settings.json file not found, some tasks may not work without this.'
+    );
     console.log(e);
 }
 
-var settings = require('./util/settingsParser')(settings_json || {}, {dest: dest});
+var settings = require('./util/settingsParser')(settings_json || {}, {
+    dest: dest,
+});
 
 module.exports = {
     browserSync: {
         server: {
             // Serve up our build folder
-            baseDir: dest
-        }
+            baseDir: dest,
+        },
     },
     browserify: {
         // A separate bundle will be generated for each
         // bundle config in the list below
         src: src,
-        bundleConfigs: [{
-            entries: src + '/javascript/main.js',
-            dest: dest + '/js',
-            outputName: 'main.js',
-            // Additional file extentions to make optional
-            extensions: ['.coffee', '.js', '.hbs'],
-            // list of modules to make require-able externally
-            //require: ['some-module', 'another-module']
-        }]
+        bundleConfigs: [
+            {
+                entries: src + '/javascript/client.js',
+                dest: dest + '/js',
+                outputName: 'client.js',
+                // Additional file extentions to make optional
+                extensions: ['.js', '.jsx'],
+                // list of modules to make require-able externally
+                //require: ['some-module', 'another-module']
+            },
+        ],
     },
     assets: {
         src: src + '/assets/**',
-        dest: dest
+        dest: dest,
     },
     images: {
         src: src + '/images/**',
-        dest: dest + '/images'
+        dest: dest + '/images',
     },
     markup: {
         src: src + '/html/**/*.html',
-        dest: dest
+        dest: dest,
     },
     sass: {
         src: src + '/sass/**/*.{sass,scss}',
@@ -52,13 +58,13 @@ module.exports = {
             // Required if you want to use SASS syntax
             // See https://github.com/dlmanning/gulp-sass/issues/81
             sourceComments: 'map',
-            imagePath: '/images' // Used by the image-url helper
-        }
+            imagePath: '/images', // Used by the image-url helper
+        },
     },
     production: {
         cssSrc: dest + '/css/*.css',
         jsSrc: dest + '/js/*.js',
-        dest: dest
+        dest: dest,
     },
-    settings: settings
+    settings: settings,
 };
